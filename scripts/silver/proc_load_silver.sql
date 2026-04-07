9a6feb55c91ec7f0f7dbc96e57c958227d71ae59
Ayush Patel <141278535+AyushPatel04@users.noreply.github.com>
2026-04-07 15:49:25 -0500

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -63,6 +63,7 @@ BEGIN
         SELECT *, 
             ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
         FROM bronze.crm_cust_info
+		WHERE cst_id IS NOT NULL
         )t WHERE flag_last = 1 -- Select the most recent record per customer
         SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds'
